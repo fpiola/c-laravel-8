d7e32c8ef9ae93646e570466231bbce3a7d16990
fpiola <francis.piola@yahoo.com.br>
2021-01-28 15:00:04 -0300

    Inserir Registros

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,22 @@ class PostController extends Controller
         return view('admin.posts.index', compact('posts'));
         //return view('admin.posts.index', ['posts' => $posts]);
     }
+
+    public function create(){
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $requeste){
+        //$requeste = new Request();
+        //dd($requeste->title); //Título
+        //dd($requeste->all()); //Todos
+
+        //Inserir no banco de dados
+        Post::create($requeste->all());
+        //(Importante) colocar o fillable no model Post
+
+        //return 'Okay';
+        return redirect()->route('posts.index');
+
+    }
 }
