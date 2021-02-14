94f7ad4dae63dc10d05bf6b724a51abd3d251be4
fpiola <francis.piola@yahoo.com.br>
2021-02-14 13:36:24 -0300

    Autenticação

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,35 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::put('posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::get('posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/{id}',[PostController::class, 'show'])->name('posts.show');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-
+Route::middleware(['auth'])->group(function () {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::put('posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+require __DIR__ . '/auth.php';
